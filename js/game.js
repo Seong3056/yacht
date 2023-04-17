69d77e868df35685a166e5f1ff73d1d7aef6e800
seong <aktnf207@naver.com>
2023-04-17 18:37:29 +0900

    주사위 랜덤각도

--- js/game.js
+++ js/game.js
@@ -1,4 +1,3 @@
-
 const $main = document.getElementById('main');
 // const $game = $main.getElementById('game');
 const $roll = document.getElementById('roll');
@@ -8,22 +7,23 @@ const $btn = $main.querySelector('button');
 $btn.onclick = () => {
     var arr = new Array(5);
     $roll.textContent = '';
-    for(var i=0;i<5;i++){        //주사위 5개 굴리기
-            arr[i] =  Math.floor((Math.random()*6)+1) ;
-            const $dice = document.createElement('img');
-            $dice.setAttribute('class',`${arr[i]}`);
-            $dice.setAttribute('src',`../img/${arr[i]}.png`);
-            $dice.setAttribute('id',`dice`);
-            
-            $roll.appendChild($dice);
-            $dice.style.position = 'absolute';
-            $dice.style.top = `${Math.random()*83}%`;
-            $dice.style.left = `${Math.random()*84}%`;
-            // $dice.style.top = `83%`;
-            // $dice.style.left = `84%`;
-            
-            $dice.style.transform = `rotate(${Math.random()*720})`;
-            $dice.style.transition = `${Math.random()*3}s`;
+    for (var i = 0; i < 5; i++) { //주사위 5개 굴리기
+        arr[i] = Math.floor((Math.random() * 6) + 1);
+        const $dice = document.createElement('img');
+        $dice.setAttribute('class', `${arr[i]}`);
+        $dice.setAttribute('src', `../img/${arr[i]}.png`);
+        $dice.setAttribute('id', `dice`);
+
+        $roll.appendChild($dice);
+        $dice.style.position = 'absolute';
+        $dice.style.top = `${Math.random()*83}%`;
+        $dice.style.left = `${Math.random()*84}%`;
+        // $dice.style.top = `83%`;
+        // $dice.style.left = `84%`;
+
+        $dice.style.transform = `rotate(${Math.random()*720}deg)`;
+        $dice.style.transition = `${Math.random()*3}s`;
+        
     }
-    
+
 }
